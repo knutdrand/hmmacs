6582e403dfe91e4353ba02870decc9635b053432
Knut Rand <knutdrand@gmail.com>
2020-10-22 15:42:38 +0200

    forward pass passes

--- hmmacs/sparse/sparsebase.py
+++ hmmacs/sparse/sparsebase.py
@@ -35,21 +35,40 @@ class _BaseSparseHMM(_BaseHMM):
 
     def _do_forward_pass(self, framelogprob, lengths):
         n_samples, n_components = framelogprob.shape
-        matrices = self.transmat_[None, ...] * np.exp(framelogprob[..., None]) #add f_i to row i of t
+        matrices = self.transmat_[None, ...] * np.exp(framelogprob[:, None, : ]) #add f_i to row i of t
+        tmp = (self.startprob_ * np.exp(framelogprob[0]))[None, :]
         ms = diagonalize(matrices)
-        lps, lds, lp_invs = (np.log(np.abs(m)) for m in ms)
+        for p, d, r, matrix in zip(ms[0], ms[1], ms[2], matrices):
+            assert np.allclose(p@np.diag(d)@r, matrix), (p@np.diag(d)@r, matrix)
+        lps, ilds, lp_invs = (np.log(np.abs(m)) for m in ms)
         sps, sds, sp_invs = (np.sign(m) for m in ms)
-        print(lds)
-        print(lengths)
-        lds *= lengths
+
+        tmp_v = np.array([[-3., -7.]])
+        for t in range(n_samples):
+            lp, ld, lr = (lps[t], ilds[t], lp_invs[t])
+            sp, sd, sr = (sps[t], sds[t], sp_invs[t])
+
+            for j in range(2):
+                v_j = logsumexp([tmp_v[0, c] + lp[c, k]+ld[k]+lr[k, j] for k in range(2) for c in range(2)],
+                                b=[sp[c, k]*sd[k]*sr[k, j] for k in range(2) for c in range(2)])
+                w_j = np.log(np.exp(tmp_v) @ matrices[t])[0, j]
+                assert np.allclose(v_j, w_j), (v_j, w_j)
+                for i in range(2):
+                    m_ij = logsumexp([lp[i, k]+ld[k]+lr[k, j] for k in range(2)], 
+                                     b=[sp[i, k]*sd[k]*sr[k, j] for k in range(2)])
+                    assert np.allclose(m_ij, np.log(matrices[t, i, j])), (m_ij, np.log(matrices[t, i, j]))
+                    
+
+        lds = ilds*lengths
+        lds[0] -= ilds[0]
         fwdlattice = np.zeros((n_samples, n_components))
-        lv = log_mask_zero(self.startprob_)
+        lv = log_mask_zero(self.startprob_)+framelogprob[0]
         for t in range(n_samples):
             lp, ld, lr = (lps[t], lds[t], lp_invs[t])
             sp, sd, sr = (sps[t], sds[t], sp_invs[t])
-            for i in range(2):
-                fwdlattice[t, i] = logsumexp([lv[j] + lp[j, k]+ld[k]+lr[k, i] for k in range(2) for j in range(2)], 
-                                             b=[sp[j, k]*sd[k]*sr[k, i] for k in range(2) for j in range(2)])
+            for j in range(2):
+                fwdlattice[t, j] = logsumexp([lv[c] + lp[c, k]+ld[k]+lr[k, j] for k in range(2) for c in range(2)], 
+                                             b=[sp[c, k]*sd[k]*sr[k, j] for k in range(2) for c in range(2)])
             lv = fwdlattice[t]
         with np.errstate(under="ignore"):
             return logsumexp(fwdlattice[-1]), fwdlattice
--- tests/sparse/test_poisson.py
+++ tests/sparse/test_poisson.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+from scipy.special import logsumexp
 from hmmacs.sparse.poissonhmm import PoissonHMM
 from hmmacs.dense.poissonhmm import PoissonHMM as DensePoissonHMM
 
@@ -9,7 +10,7 @@ def model():
     m.startprob_ = np.array([0.2, 0.8])
     m.transmat_ = np.array([[0.3, 0.7],
                             [0.1, 0.9]])
-    m.rate_ = np.array([2., 1.])
+    m.rate_ = np.array([2., 9.])
     return m
 
 @pytest.fixture
@@ -18,28 +19,32 @@ def dense_model():
     m.startprob_ = np.array([0.2, 0.8])
     m.transmat_ = np.array([[0.3, 0.7],
                             [0.1, 0.9]])
-    m.rate_ = np.array([2., 1.])
+    m.rate_ = np.array([2., 9.])
     return m
 
 @pytest.fixture
-def dense_X():
-    return np.asanyarray(np.concatenate([np.ones(4)*3, np.ones(6)*10]*3).reshape((-1, 1)), dtype="int")
-
-@pytest.fixture
 def X():
-    return np.array([3, 10]*3, dtype="int").reshape((-1, 1))
+    return np.array([0, 1,2,3] + [3, 10]*3, dtype="int").reshape((-1, 1))
 
 @pytest.fixture
 def lengths():
-    return np.array([4, 6]*3, dtype="int").reshape((-1, 1))
+    return np.array([1, 1,1,1] + [4, 6]*3, dtype="int").reshape((-1, 1))
 
-def test_score(X, lengths, model, dense_X, dense_model):
+def get_dense_X(X, lengths):
+    return np.concatenate([[x]*l for x, l in zip(X.flatten(), lengths.flatten())]).reshape((-1, 1))
+
+def test_score(X, lengths, model, dense_model):
+    dense_X = get_dense_X(X, lengths)
     true = dense_model.score(dense_X)
     sparse = model.score(X, lengths)
     assert np.allclose(sparse, true)
 
-def _test_forward_pass(X, lengths, model, dense_X, dense_model):
-    _, true = dense_model._do_forward_pass(dense_X)
+def test_forward_pass(X, lengths, model, dense_model):
+    dense_X = get_dense_X(X, lengths)
+    _, true = dense_model._do_forward_pass(dense_model._compute_log_likelihood(dense_X))
     ts = np.cumsum(lengths)-1
-    _, sparse = model._do_forward_pass(X, lengths)
+    _, sparse = model._do_forward_pass(model._compute_log_likelihood(X), lengths)
+    for t in zip(true[ts], sparse, X, lengths):
+        print(t)
+
     assert np.allclose(sparse, true[ts])
